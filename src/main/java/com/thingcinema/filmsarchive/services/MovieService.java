3f2f61e0e93a1212bda36e9d4d488ba28d25774d
DOBREV <dobrev8444@gmail.com>
2018-09-04 13:56:55 +0300

    interface MovieService updated and class MovieServiceImpl created

--- src/main/java/com/thingcinema/filmsarchive/services/MovieService.java
+++ src/main/java/com/thingcinema/filmsarchive/services/MovieService.java
@@ -1,10 +1,16 @@
 package com.thingcinema.filmsarchive.services;
 
 
+import com.thingcinema.filmsarchive.models.Movie;
+
 import java.util.List;
 
 public interface MovieService {
 
+    void createMovie(Movie movie);
     List<Movie> getMovies();
+    Movie getMovieByName(String name);
+    void updateMovie(String name, Movie movie);
+    void deleteMovie(String name);
 
 }
---
+++ src/main/java/com/thingcinema/filmsarchive/services/MovieServiceImpl.java
@@ -0,0 +1,32 @@
+package com.thingcinema.filmsarchive.services;
+
+import com.thingcinema.filmsarchive.models.Movie;
+
+import java.util.List;
+
+public class MovieServiceImpl implements MovieService {
+    @Override
+    public void createMovie(Movie movie) {
+        
+    }
+
+    @Override
+    public List<Movie> getMovies() {
+        return null;
+    }
+
+    @Override
+    public Movie getMovieByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void updateMovie(String name, Movie movie) {
+
+    }
+
+    @Override
+    public void deleteMovie(String name) {
+
+    }
+}
