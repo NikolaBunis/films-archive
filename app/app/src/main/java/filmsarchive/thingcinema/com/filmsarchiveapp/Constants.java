75683b87b22135827144b93a45388e4c0802bd18
NikolaBunis <bunisbg@gmail.com>
2018-09-05 14:56:26 +0300

    Added a constants class and a Movie model.

---
+++ app/app/src/main/java/filmsarchive/thingcinema/com/filmsarchiveapp/Constants.java
@@ -0,0 +1,8 @@
+package filmsarchive.thingcinema.com.filmsarchiveapp;
+
+public class Constants {
+    public static final String BASE_SERVER_URL
+            = "http://192.168.2.109:8080/api";
+
+
+}
---
+++ app/app/src/main/java/filmsarchive/thingcinema/com/filmsarchiveapp/models/Movie.java
@@ -0,0 +1,49 @@
+package filmsarchive.thingcinema.com.filmsarchiveapp.models;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+
+    private int id;
+    private String name;
+    private String description;
+    private String urlImage;
+
+    public Movie(String name, String description, String urlImage) {
+        this.name = name;
+        this.description = description;
+        this.urlImage = urlImage;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
