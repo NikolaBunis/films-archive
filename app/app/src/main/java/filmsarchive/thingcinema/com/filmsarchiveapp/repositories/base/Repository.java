afaccc58b3b0c743b1291f2b968d1c883640c55a
DOBREV <dobrev8444@gmail.com>
2018-09-05 16:11:01 +0300

    Revert "Revert "package repositories created with package base (interface MovieService) and class HttpMovieService""
    This reverts commit 2b51d12b7795bb0db50ea6d1909f8948f56938b3.

---
+++ app/app/src/main/java/filmsarchive/thingcinema/com/filmsarchiveapp/repositories/base/Repository.java
@@ -0,0 +1,15 @@
+package filmsarchive.thingcinema.com.filmsarchiveapp.repositories.base;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface Repository<T> {
+
+    List<T> getAll() throws IOException;
+
+    T add(T item) throws IOException;
+
+    T getById(int id) throws IOException;
+
+    T deleteById(int id) throws IOException;
+}
