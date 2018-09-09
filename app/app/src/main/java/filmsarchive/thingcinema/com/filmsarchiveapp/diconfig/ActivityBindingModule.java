4af9c4836eacc3acc5cc419a2159fecdcb381ec4
NikolaBunis <bunisbg@gmail.com>
2018-09-09 16:18:32 +0300

    Deletion and addition form activities added

--- app/app/src/main/java/filmsarchive/thingcinema/com/filmsarchiveapp/diconfig/ActivityBindingModule.java
+++ app/app/src/main/java/filmsarchive/thingcinema/com/filmsarchiveapp/diconfig/ActivityBindingModule.java
@@ -3,7 +3,10 @@ package filmsarchive.thingcinema.com.filmsarchiveapp.diconfig;
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 import filmsarchive.thingcinema.com.filmsarchiveapp.views.HomePage.HomePageActivity;
+import filmsarchive.thingcinema.com.filmsarchiveapp.views.MovieArchive.AdditionFormActivity.AdditionFormActivity;
+import filmsarchive.thingcinema.com.filmsarchiveapp.views.MovieArchive.DeletionFormActivity.DeletionFormActivity;
 import filmsarchive.thingcinema.com.filmsarchiveapp.views.MovieArchive.MovieArchiveActivity;
+import filmsarchive.thingcinema.com.filmsarchiveapp.views.MovieArchive.MovieInfoActivity;
 
 @Module
 public abstract class ActivityBindingModule {
@@ -15,7 +18,17 @@ public abstract class ActivityBindingModule {
     @ContributesAndroidInjector()
     abstract MovieArchiveActivity movieArchiveActivity();
 
+    @ActivityScoped
+    @ContributesAndroidInjector()
+    abstract MovieInfoActivity  movieInfoActivity();
 
+    @ActivityScoped
+    @ContributesAndroidInjector()
+    abstract AdditionFormActivity additionFormActivity();
+
+    @ActivityScoped
+    @ContributesAndroidInjector()
+    abstract DeletionFormActivity deletionFormActivity();
     /*
     @ActivityScoped
     @ContributesAndroidInjector(modules = StatisticsModule.class)
